2fd86b1825b0582f8329f9c696c05fe55baea5d9
dileepumar <chokkakulakumar1996@gmail.com>
2024-06-18 18:02:16 +0530

    code

---
+++ proj1/src/demo.java
@@ -0,0 +1,9 @@
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("my Demo program");
+        System.err.println("hello class");
+        System.out.println("java demo program");
+
+    }
+    
+}
